6c0fd98e6e864171832409f6c48c64e76c8aea7d
unknown <shahnazajmeri@gmail.com>
2019-11-24 14:45:38 -0500

    removing proxy from package and adding setupproxy for local and node server.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,21 +5,21 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 
-module.exports = function(app) {
+module.exports = function(app, express, bodyParser, path) {
     app.use(proxy('/api/**', { target: 'http://localhost:8080' }));
     // app.use(proxy('/otherApi/**', { target: 'http://localhost:5000' }));
 
-    // app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static(path.join(__dirname, 'build')));
 
-    // app.get('/ping', function (req, res) {
-    // return res.send('pong');
-    // });
+    app.get('/ping', function (req, res) {
+        return res.send('pong');
+    });
 
-    // app.get('/', function (req, res) {
-    // res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    // });
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
 
-    // app.listen(process.env.PORT || 8080);
+    app.listen(process.env.PORT || 8080);
 };
 
 
