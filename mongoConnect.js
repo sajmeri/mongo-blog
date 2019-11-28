c5fec9bc123388fc9a457bdb79fb3fbf89aa8985
unknown <shahnazajmeri@gmail.com>
2019-11-28 07:13:06 -0500

    mongodb connect

--- mongoConnect.js
+++ mongoConnect.js
@@ -1,9 +1,14 @@
 //This is just a sample from the cluster - not used in the code yet
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://admin:<password>@react-blog-fibv0.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://admin:ca2000@react-blog-fibv0.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
+client.connect((err, uri) => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', uri);
+  }
   client.close();
 });
